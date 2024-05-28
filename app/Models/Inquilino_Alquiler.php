aff713497e6c13c8ede854ebd36b2b773ef2857a
sandro3210 <163931466+sandro3210@users.noreply.github.com>
2024-05-28 08:37:37 -0500

    Update Inquilino_Alquiler.php

--- app/Models/Inquilino_Alquiler.php
+++ app/Models/Inquilino_Alquiler.php
@@ -1,11 +1,13 @@
 <?php
-
 namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Inquilino_Alquiler extends Model
+class InquilinoAlquiler extends Model
 {
-    use HasFactory;
+  protected $table = 'inquilino_alquiler';
+
+  protected $fillable = [
+    'inquilino_id',
+    'alquiler_id'
+  ];
 }
